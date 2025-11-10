39c1008f7da34cbf4d665c865c949be5139d3b36
Arthur Vincent Simon <developarvin@gmail.com>
2025-11-10 20:00:37 +0800

    Add feature collection for travel map data

--- src/routes/map.tsx
+++ src/routes/map.tsx
@@ -1,11 +1,23 @@
 import { createFileRoute } from '@tanstack/react-router'
-import Map from 'react-map-gl/maplibre';
+import Map, { Source, Layer } from 'react-map-gl/maplibre';
 import 'maplibre-gl/dist/maplibre-gl.css';
 import { useState } from 'react';
+import type {FeatureCollection} from 'geojson';
+
+import geojsonData from '@/data/my_travel.json';
 
 export const Route = createFileRoute('/map')({
   component: RouteComponent,
 })
+
+const layerStyle = {
+  id: 'point',
+  type: 'circle' as const,
+  paint: {
+    'circle-radius': 10,
+    'circle-color': '#007cbf'
+  }
+};
 
 function RouteComponent() {
   const [viewState, setViewState] = useState({
@@ -19,6 +31,10 @@ function RouteComponent() {
       {...viewState}
       style={{width: '100vw', height: '100vh'}}
       mapStyle="https://demotiles.maplibre.org/globe.json"
-    />
+    >
+       <Source id="my-data" type="geojson" data={geojsonData as FeatureCollection}>
+        <Layer {...layerStyle} />
+      </Source>
+    </Map>
   )
 } 
