c65988da2c26fae535f8c7aaad0686a41dd70bc5
Arthur Vincent Simon <developarvin@gmail.com>
2025-11-14 12:51:45 +0800

    Add code for github action to deploy project to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/travelled-map/',
   plugins: [
     tanstackRouter({
       target: 'react',
